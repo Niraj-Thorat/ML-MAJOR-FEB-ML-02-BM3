5691b80ac5aa29b93b96aa91e5c736d1d970227e
Niraj-Thorat <81378275+Niraj-Thorat@users.noreply.github.com>
2021-04-17 15:29:49 +0530

    Update Sentiments.py

--- Sentiments.py
+++ Sentiments.py
@@ -4,6 +4,6 @@ import joblib
 model = joblib.load('Tweets')
 st.title('Tweets Classifier')
 ip = st.text_input('Enter your message')
-op = model.predict([ip])
+op = model.predict([[ip]])
 if st.button('Predict'):
   st.title(op[0]) 
